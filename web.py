eba214a994f7de0b1c245d9a7e0cf14ccd0da71a
Nicholas Potter <potter.nicholas@gmail.com>
2014-03-26 15:49:22 -0700

    Working get_file method

--- web.py
+++ web.py
@@ -2,10 +2,10 @@
 Methods for handling web-based things.
 """
 
-from url.request import urlretrieve
+from urllib.request import urlretrieve
 
 
-get_file(url, saveto=False):
+def get_file(url, saveto=False):
     """Pulls the file and saves to the local drive, optionally with 
     the name specified by saveto."""
     request = urlretrieve(url, saveto)
