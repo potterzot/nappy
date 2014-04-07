28e6b12fdb97565a675f8ddba1e7aeba44d31eda
potterzot <nick@potterzot.com>
2014-04-06 22:10:31 -0700

    add progress report to get_file method.

--- web.py
+++ web.py
@@ -5,10 +5,20 @@ Methods for handling web-based things.
 from urllib.request import urlretrieve
 
 
-def get_file(url, saveto=False):
+def get_file(url, saveto=False, verbose=False):
     """Pulls the file and saves to the local drive, optionally with 
     the name specified by saveto."""
-    request = urlretrieve(url, saveto)
+    
+    def _verbose(blk, blk_size, size):
+        """Handles the printing of the progress."""
+        print(str(round(100*blk*blk_size/size))+ "% done...")
+    
+    if verbose: verbose=_verbose
+    
+    print("Downloading file: "+url)
+    if saveto: print("... as "+saveto)
+
+    urlretrieve(url, saveto, _verbose)
     
 
 
